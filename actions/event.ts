be476b0b2f9aef3d269e2998212e025813ecb188
mguzman20 <m.gguzman@uc.cl>
2024-11-25 13:07:23 -0300

    Agregar acciones para la gestión de eventos: obtener y crear eventos en la base de datos

---
+++ actions/event.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import prisma from "@/lib/prisma"
+import type { EventStatus } from "@prisma/client"
+import type { Event } from "@/app/admin/events/form"
+
+export async function getEvents() {
+    return await prisma.event.findMany()
+}
+
+export async function createEvent(data: Event) {
+    try {
+
+        await prisma.event.create({
+            data: {
+                name: data.name,
+                description: data.description,
+                date: data.date,
+                status: data.status as EventStatus,
+                price: data.price,
+                capacity: data.capacity,
+                location: data.location,
+            }
+        })
+
+        return { success: true }
+
+    } catch (error) {
+        console.error(error)
+        return { success: false }
+    }
+}
