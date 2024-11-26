8d83a5f292c51b0d0d5c5841604a9c528972a7d2
mguzman20 <m.gguzman@uc.cl>
2024-11-26 12:03:41 -0300

    Actualizar el estilo de los botones en la página de administración de tickets y ajustar el tamaño del ícono en la barra lateral

--- app/admin/tickets/page.tsx
+++ app/admin/tickets/page.tsx
@@ -65,7 +65,7 @@ export default async function Component() {
                 <TableCell>
                   <DropdownMenu>
                     <DropdownMenuTrigger>
-                      <Button>
+                      <Button variant={'outline'}>
                         <MoreHorizontal />
                       </Button>
                     </DropdownMenuTrigger>
